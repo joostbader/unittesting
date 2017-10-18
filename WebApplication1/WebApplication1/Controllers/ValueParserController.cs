92c27bc0aea14ca576293c20d2597d880e10c374
Joost Bader <joostbader@hotmail.com>
2017-10-18 11:27:34 +0000

    added files

---
+++ WebApplication1/WebApplication1/Controllers/ValueParserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ValueParserController : Controller
+    {
+        // GET: ValueParser
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ValueParser model)
+        {
+            return View();
+        }
+    }
+}
